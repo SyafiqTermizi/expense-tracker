95e0f1670c6cdad8fdbf3bc7495d925644e8eca1
Syafiq Termizi <ahmadsyafiq93@gmail.com>
2023-05-24 00:15:57 +0800

    Simplify dahsboard query

--- expense/accounts/views.py
+++ expense/accounts/views.py
@@ -8,35 +8,13 @@ from .models import AccountType, Account, AccountAction
 
 @login_required
 def dashboard_view(request: HttpRequest) -> HttpResponse:
-    try:
-        income_account = Account.objects.filter(
-            belongs_to=request.user,
-            action__account_type__type__in=[AccountType.Type.INCOME],
-        ).latest()
-    except Account.DoesNotExist:
-        latest_income = 0
-    else:
-        latest_income = income_account.amount
-
-    try:
-        saving_account = Account.objects.filter(
-            belongs_to=request.user,
-            action__account_type__type__in=[AccountType.Type.SAVING],
-        ).latest()
-    except Account.DoesNotExist:
-        latest_saving = 0
-    else:
-        latest_saving = saving_account.amount
-
-    try:
-        cash_account = Account.objects.filter(
-            belongs_to=request.user,
-            action__account_type__type__in=[AccountType.Type.CASH],
-        ).latest()
-    except Account.DoesNotExist:
-        latest_cash = 0
-    else:
-        latest_cash = cash_account.amount
+    accounts = {}
+    for balance in (
+        Account.objects.order_by("action__account_type__type", "-created_at")
+        .distinct("action__account_type__type")
+        .values("amount", "action__account_type__type")
+    ):
+        accounts[balance["action__account_type__type"].title()] = balance["amount"]
 
     actions = AccountAction.objects.filter(belongs_to=request.user).order_by(
         "-created_at"
@@ -46,11 +24,7 @@ def dashboard_view(request: HttpRequest) -> HttpResponse:
         request,
         "accounts/dashboard.html",
         context={
-            "accounts": {
-                "income": latest_income,
-                "saving": latest_saving,
-                "cash": latest_cash,
-            },
+            "accounts": accounts,
             "actions": actions,
         },
     )
