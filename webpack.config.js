c02193741e833d1ef0df21cc06f5b6d00a9c13c2
Syafiq Termizi <73152242+SyafiqTermizi2010@users.noreply.github.com>
2023-05-28 15:23:43 +0800

    Setup svelte with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
-var path = require('path');
+// https://github.com/baileyherbert/svelte-webpack-starter
 
+const path = require('path');
+const SveltePreprocess = require('svelte-preprocess');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
@@ -7,11 +9,22 @@ module.exports = {
     entry: {
         style: "./expense_fe/js/style.js",
     },
+    resolve: {
+        alias: {
+            // Note: Later in this config file, we'll automatically add paths from `tsconfig.compilerOptions.paths`
+            svelte: path.resolve('node_modules', 'svelte')
+        },
+        extensions: ['.mjs', '.js', '.ts', '.svelte'],
+        mainFields: ['svelte', 'browser', 'module', 'main'],
+        conditionNames: ['svelte', 'browser']
+    },
     output: {
         path: path.resolve('./static/'),
         filename: '[name].js',
     },
-    plugins: [new MiniCssExtractPlugin()],
+    plugins: [
+        new MiniCssExtractPlugin(),
+    ],
     module: {
         rules: [
             {
@@ -23,6 +36,35 @@ module.exports = {
                     "sass-loader",
                 ],
                 exclude: /node_modules/,
+            },
+            {
+                test: /\.svelte$/,
+                use: {
+                    loader: 'svelte-loader',
+                    options: {
+                        compilerOptions: {
+                            // Dev mode must be enabled for HMR to work!
+                            dev: true
+                        },
+                        emitCss: false,
+                        hotReload: true,
+                        hotOptions: {
+                            // List of options and defaults: https://www.npmjs.com/package/svelte-loader-hot#usage
+                            noPreserveState: false,
+                            optimistic: true,
+                        },
+                        preprocess: SveltePreprocess({
+                            scss: false,
+                            sass: false,
+                        })
+                    }
+                }
+            },
+            {
+                test: /node_modules\/svelte\/.*\.mjs$/,
+                resolve: {
+                    fullySpecified: false
+                }
             },
         ]
     }
