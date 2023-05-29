9edd3be0dd9bf27d17e1110e77cb655b605714ba
Syafiq Termizi <ahmadsyafiq93@gmail.com>
2023-05-29 22:26:47 +0800

    Reduce n+1 query to 3 query max on dashboard

--- expense/dashboard/views.py
+++ expense/dashboard/views.py
@@ -7,11 +7,14 @@ from django.utils import timezone
 
 from expense.accounts.models import Account, AccountAction
 
-from .serializers import AccountActionAndExpenseSerializer
-
 
 @login_required
 def dashboard_view(request: HttpRequest) -> HttpResponse:
+    """
+    Returns an overview of the user's account balance and spending activities
+    """
+
+    # 1. Get account balance
     account_balance = {}
     for balance in (
         Account.objects.order_by(
@@ -23,19 +26,32 @@ def dashboard_view(request: HttpRequest) -> HttpResponse:
     ):
         account_balance[balance["action__account_type__type"]] = balance["amount"]
 
+    # 2. Get account activities
+    account_actions = (
+        AccountAction.objects.filter(
+            belongs_to=request.user,
+            created_at__gte=(timezone.now() - timedelta(days=30)),
+        )
+        .order_by("-created_at")
+        .select_related("expense", "account_type")
+    )
+
+    activities = list(
+        map(
+            lambda acc_act: {
+                "expense": hasattr(acc_act, "expense"),
+                "account_type": str(acc_act.account_type),
+                "created_at": acc_act.created_at.strftime("%d-%m-%Y"),
+                "description": acc_act.description,
+                "amount": acc_act.amount,
+                "action": acc_act.action,
+            },
+            account_actions,
+        )
+    )
+
     return render(
         request,
         "dashboard/dashboard.html",
-        context={
-            "balance": account_balance,
-            "activities": AccountActionAndExpenseSerializer(
-                AccountAction.objects.filter(
-                    belongs_to=request.user,
-                    created_at__gte=(timezone.now() - timedelta(days=30)),
-                )
-                .order_by("-created_at")
-                .select_related("expense"),
-                many=True,
-            ).data,
-        },
+        context={"balance": account_balance, "activities": activities},
     )
