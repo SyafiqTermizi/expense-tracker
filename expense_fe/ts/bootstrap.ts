caf95349b1a89e461fc103bf0f326f59b14ede35
Syafiq Termizi <ahmadsyafiq93@gmail.com>
2023-06-21 23:15:04 +0800

    Use raw js to trigger dropdown

--- expense_fe/ts/bootstrap.ts
+++ expense_fe/ts/bootstrap.ts
@@ -1,2 +1,27 @@
-import { Collapse } from "bootstrap/js/dist/collapse";
-import { Dropdown } from "bootstrap/js/dist/dropdown";
+const profileButton = document.getElementById("profile-button");
+const profileDropdown = document.getElementById("profile-dropdown");
+
+profileButton.addEventListener("click", () => {
+    if (profileButton.classList.contains("show")) {
+        profileButton.classList.remove("show");
+        profileDropdown.classList.remove("show");
+    } else {
+        profileButton.classList.add("show");
+        profileDropdown.classList.add("show");
+    }
+});
+
+
+const navbarToggleButton = document.getElementById("navbar-toggle-button");
+const navbarMobileMenu = document.getElementById("navbar-mobile-menu");
+
+navbarToggleButton.classList.add("collapsed");
+navbarToggleButton.addEventListener("click", () => {
+    if (navbarToggleButton.classList.contains("collapsed")) {
+        navbarToggleButton.classList.remove("collapsed");
+        navbarMobileMenu.classList.add("show");
+    } else {
+        navbarToggleButton.classList.add("collapsed");
+        navbarMobileMenu.classList.remove("show");
+    }
+});
