a00bf84c3fdd26226b45f15f2239aae366c5d0e3
Syafiq Termizi <ahmadsyafiq93@gmail.com>
2023-07-26 23:14:13 +0800

    I don't like serving static file with python :(, but I need this fast

--- docker/expense/start-prod.sh
+++ docker/expense/start-prod.sh
@@ -5,5 +5,6 @@ set -o pipefail
 set -o nounset
 
 export DJANGO_SETTINGS_MODULE=expense.settings.prod
+/usr/local/bin/python3 /app/manage.py collectstatic --noinput
 /usr/local/bin/python3 /app/manage.py migrate --noinput
 /usr/local/bin/gunicorn expense.wsgi -c ./gunicorn.conf.py
--- expense/settings/prod.py
+++ expense/settings/prod.py
@@ -1,3 +1,13 @@
 from .base import *
 
 DEBUG = False
+
+MIDDLEWARE = list(
+    set(
+        [
+            "django.middleware.security.SecurityMiddleware",
+            "whitenoise.middleware.WhiteNoiseMiddleware",
+            *MIDDLEWARE,
+        ]
+    )
+)
