8ad1bc372e25061ea566bff599823f55f773f6a9
Syafiq Termizi <ahmadsyafiq93@gmail.com>
2023-07-29 16:23:09 +0800

    Setup sentry for logging

--- expense/settings/prod.py
+++ expense/settings/prod.py
@@ -1,3 +1,6 @@
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 from .base import *
 
 DEBUG = False
@@ -16,3 +19,11 @@ STORAGES = {
 }
 
 WHITENOISE_MAX_AGE = 86400  # 24 hours
+
+
+sentry_sdk.init(
+    dsn=os.environ.get("SENTRY_DSN"),
+    integrations=[DjangoIntegration()],
+    traces_sample_rate=1.0,
+    send_default_pii=True,
+)
