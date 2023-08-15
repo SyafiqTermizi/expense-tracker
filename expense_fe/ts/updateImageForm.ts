7990c972193f5ae0a1ababdeeb40dab6f2f92218
Syafiq Termizi <ahmadsyafiq93@gmail.com>
2023-08-15 22:38:23 +0800

    Changes

--- expense_fe/ts/updateImageForm.ts
+++ expense_fe/ts/updateImageForm.ts
@@ -2,6 +2,7 @@ const imageInput = document.getElementById("imageInput") as HTMLInputElement;
 const imagePreview = document.getElementById("imagePreview") as HTMLImageElement;
 const imageLink = document.getElementById("imageLink") as HTMLAnchorElement;
 const imageRemoveButton = document.getElementById("imageRemoveButton");
+const imageContainer = document.getElementById("imageContainer");
 const existingImageSrc = imagePreview.src;
 
 imageInput.onchange = _ => {
@@ -10,12 +11,16 @@ imageInput.onchange = _ => {
         imagePreview.classList.remove("d-none");
         imagePreview.src = URL.createObjectURL(file);
         imageLink.href = URL.createObjectURL(file);
+        imageContainer.classList.add("input-group");
+        imageRemoveButton.classList.remove("d-none");
     }
 }
 
 imageRemoveButton.onclick = _ => {
     imagePreview.src = existingImageSrc;
     imageLink.href = existingImageSrc;
+    imageContainer.classList.remove("input-group");
+    imageRemoveButton.classList.add("d-none");
     imageInput.files = null;
     imageInput.value = "";
 }
