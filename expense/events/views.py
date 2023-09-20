41b79710487e5395bf9a5a6f827901a6002cd69b
Syafiq Termizi <73152242+SyafiqTermizi2010@users.noreply.github.com>
2023-09-20 09:13:41 +0800

    Show total expense in event detail page

--- expense/events/views.py
+++ expense/events/views.py
@@ -1,6 +1,7 @@
 from typing import Any, Dict
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Sum
 from django.db.models.query import QuerySet
 from django.urls import reverse_lazy
 from django.views.generic import (
@@ -36,6 +37,21 @@ class DetailEventView(LoginRequiredMixin, DetailView):
 
     def get_queryset(self) -> QuerySet[Any]:
         return self.request.user.events.all()
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        total_expense = (
+            self.object.expense_set.values("event")
+            .annotate(total_expense=Sum("expense__amount"))
+            .order_by("total_expense")
+            .values("total_expense")
+            .first()
+        ) or {"total_expense": 0}
+
+        return {
+            "expense": total_expense,
+            "expenses": self.object.expense_set.select_related("expense"),
+            **super().get_context_data(**kwargs),
+        }
 
 
 class ListEventView(LoginRequiredMixin, ListView):
